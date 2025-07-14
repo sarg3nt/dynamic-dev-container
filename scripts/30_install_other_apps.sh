8cb0ecf5deb128a2043d089a87350d7562736547
David Sargent <dave_sargent@selinc.com>
2025-07-14 08:37:06 -0700

    Move installation to init and clean up files

---
+++ scripts/30_install_other_apps.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# cSpell:ignore kubectx kubens mvdan gofumpt FUMPT gopls
+
+main() {
+  source "/usr/bin/lib/sh/log.sh"
+  export PATH="$HOME/.local/share/mise/shims:$HOME/.local/bin/:$PATH"
+
+  log "30_install_other_apps.sh" "blue"
+
+  add_vscode_extensions_cache
+  add_bash_history_cache
+  add_fzf_completions_files
+  install_kubectx_kubens_completions
+  install_omz_plugins
+  cleanup
+  date +"%B %d %Y" >/home/vscode/build_date.txt
+}
+
+add_vscode_extensions_cache() {
+  log "Adding VSCode Extensions Cache Support" "green"
+  mkdir -p "/home/${USERNAME}/.vscode-server/extensions"
+  chown -R "${USERNAME}" "/home/${USERNAME}/.vscode-server"
+}
+
+add_bash_history_cache() {
+  log "Adding Bash History Cache Support" "blue"
+  sudo mkdir /commandhistory
+  sudo touch /commandhistory/.bash_history
+  sudo chown -R "$USERNAME" "/commandhistory"
+}
+
+add_fzf_completions_files() {
+  log "Adding FZF Completions Files" "green"
+  curl -sL https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh --output "$HOME/.fzf-key-bindings.zsh"
+  curl -sL https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh --output "$HOME/.fzf-completion.zsh"
+}
+
+install_kubectx_kubens_completions() {
+  log "Installing kubectx and kubens completions" "green"
+  mkdir -p "$HOME/.oh-my-zsh/custom/completions"
+  chmod -R 755 "$HOME/.oh-my-zsh/custom/completions"
+
+  log "Installing kubectx completions" "green"
+  curl -sL https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubectx.zsh --output "$HOME/.oh-my-zsh/custom/completions/_kubectx.zsh"
+  log "Installing kubens completions" "green"
+  curl -sL https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubens.zsh --output "$HOME/.oh-my-zsh/custom/completions/_kubens.zsh"
+}
+
+install_omz_plugins() {
+  log "Installing Oh My ZSH plugins" "green"
+  git clone --depth 1 -- https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  git clone --depth 1 -- https://github.com/zdharma-continuum/fast-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
+  # https://github.com/marlonrichert/zsh-autocomplete/issues/797
+  git clone --branch "24.09.04" --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete"
+  git clone --depth 1 -- https://github.com/zsh-users/zsh-completions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
+}
+
+cleanup() {
+  log "Running cleanup" "blue"
+  log "Deleting files from /tmp" "green"
+  sudo rm -rfv /tmp/*
+  echo ""
+
+  log "Deleting all .git directories." "green"
+  find / -path /proc -prune -o -type d -name ".git" -not -path '/.git' -exec rm -rfv {} + 2>/dev/null || true
+  echo ""
+
+  log "Deleting all data in /var/log" "green"
+  sudo rm -rfv /var/log/*
+  echo ""
+}
+
+# Run main
+if ! (return 0 2>/dev/null); then
+  (main "$@")
+fi
